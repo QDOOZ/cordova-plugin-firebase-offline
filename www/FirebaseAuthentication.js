ed162f7e4824cb6ee2dd90379ead041c3ce87560
Oli Callaghan <me@olicallaghan.com>
2019-04-29 16:42:26 +0100

    Adds bridge for reload method for Firebase auth plugin

--- www/FirebaseAuthentication.js
+++ www/FirebaseAuthentication.js
@@ -110,5 +110,10 @@ module.exports = {
         return new Promise(function(resolve, reject) {
             exec(resolve, reject, PLUGIN_NAME, "reauthenticateWithCredential", [email, password]);
         });
+    },
+    reload: function() {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, PLUGIN_NAME, "reload", [])
+        });
     }
 };
